ab04a1ee1d0c1372c88128549db15b6eb0e4c6c7
whl <scissorshand.e@qq.com>
2018-07-13 16:49:23 +0800

    配置stylus-loader,新增tab布局

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,33 @@
 <template>
   <div id="app">
+    <v-header></v-header>
     <img src="./assets/logo.png">
-    <router-view/>
+    <!-- <router-view/> -->
+    <div class="tab">
+      <div class="tab-item">商品</div>
+      <div class="tab-item">评价</div>
+      <div class="tab-item">商家</div>
+    </div>
   </div>
 </template>
 
 <script>
+import header from "./components/header/header.vue";
 export default {
-  name: 'App'
-}
+  name: "App",
+  components: {
+    "v-header": header
+  }
+};
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="stylus">
+#app
+  .tab
+    display: flex
+    width: 100%
+    line-height: 40px
+    .tab-item
+      text-align: center
+      flex: 1
 </style>
---
+++ src/components/header/header.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <a>{{msg}}</a>
+        <a>whl</a>
+    </div>
+</template>
+
+<script>
+import Vue from 'vue';
+export default {
+    name: 'header',
+    data(){
+        return{
+            msg: 'mHeader!'
+        }
+    }
+}
+</script>
+<style>
+
+</style>
